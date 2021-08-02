123ca7660ec9c7957ed1bdcfa7ac1240e14b9b65
g3or3 <>
2021-08-02 15:36:44 -0400

    Add a base endpoint to fix severe health status on Beanstalk.

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,7 @@
 module.exports = (server) => {
+	server.use("/", (req, res) => {
+		res.send("working");
+	});
 	server.use("/api/person", require("./personRoute"));
 	server.use("/api/record", require("./recordRoute"));
 };
