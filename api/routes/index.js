f3c854d0badece937ca852a4ef679ef5310420cf
g3or3 <>
2021-08-07 13:38:26 -0400

    Changed base route to satisfy health warning on Beanstalk.

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,6 @@ module.exports = (server) => {
 	server.use("/api/person", require("./personRoute"));
 	server.use("/api/record", require("./recordRoute"));
 	server.use("/", (req, res) => {
-		res.status(200);
+		res.json({ message: "Server is running." });
 	});
 };
